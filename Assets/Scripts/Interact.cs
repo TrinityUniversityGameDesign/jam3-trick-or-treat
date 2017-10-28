e958f2bc85009737e0f31c743a9c81b7de6e7753
szbidi <szbidi@trinity.edu>
2017-10-28 12:11:34 -0500

    interactions

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Interact : MonoBehaviour {
+	//static private int items;
+	public BoxCollider2D candy;
+	// Use this for initialization
+	void Start () {
+		//items = 0;
+	}
+
+	// Update is called once per frame
+//	void Update () {
+//
+//	}
+
+	void OnTriggerEnter2D(Collider2D candy){
+		//items = items + 1;
+		Destroy (candy.gameObject);
+		Debug.Log ("!!!!!!!!!!!");
+	}
+}
