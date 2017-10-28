8d72f831ee887449731a68b422e01a0cf4885cff
Samantha Ortiz <samiortiz0627@yahoo.com>
2017-10-28 14:01:31 -0500

    sounds

--- Assets/Scripts/Lightning.cs
+++ Assets/Scripts/Lightning.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Lightning : MonoBehaviour {
-	private float numFlashes = 5f;//Random.Range(2f,4f);
+	private float numFlashes = 4f;//Random.Range(2f,4f);
 	private SpriteRenderer sp;
+	AudioClip light;
+
 	
 	// Use this for initialization
 	void Start () {
 		sp = GetComponent<SpriteRenderer> ();
 		StartCoroutine (Timer());
+	
+
 	}
 	
 	// Update is called once per frame
 	IEnumerator Flash(){
+		
 		for (int i = 0; i < numFlashes; i++) {
 			sp.enabled = true;
-			yield return new WaitForSeconds (Random.value);
+			yield return new WaitForSeconds (Random.Range(0f,.2f));
 			sp.enabled = false;
-			yield return new WaitForSeconds (Random.value);
+			yield return new WaitForSeconds (Random.Range(0f,.2f));
 		}
 		sp.enabled = false;
 	}
