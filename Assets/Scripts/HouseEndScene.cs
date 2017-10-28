dee8e3a303bb041d2e2efd065fead9743206489e
Lauren Truong <lauren@laurens-mbp.trinity.local>
2017-10-28 16:40:36 -0500

    new enemy

---
+++ Assets/Scripts/HouseEndScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HouseEndScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.tag == "Player") {
+			SceneManager.LoadScene ("EndScene"); 
+		}
+	}
+
+}
