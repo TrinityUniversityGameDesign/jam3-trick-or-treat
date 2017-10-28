11a000e3c061e9ff86ecb6d9cd045d15225e8a12
Samantha Ortiz <samiortiz0627@yahoo.com>
2017-10-28 14:12:09 -0500

    title screen

--- Assets/Scripts/Lightning.cs
+++ Assets/Scripts/Lightning.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class Lightning : MonoBehaviour {
 	private float numFlashes = 4f;//Random.Range(2f,4f);
 	private SpriteRenderer sp;
-	AudioClip light;
+	private AudioSource aud;
+	public AudioClip light; 
 
 	
 	// Use this for initialization
 	void Start () {
 		sp = GetComponent<SpriteRenderer> ();
 		StartCoroutine (Timer());
+		aud = GetComponent<AudioSource> ();
 	
 
 	}
 	
 	// Update is called once per frame
 	IEnumerator Flash(){
-		
+		aud.PlayOneShot(light, 1);
 		for (int i = 0; i < numFlashes; i++) {
 			sp.enabled = true;
 			yield return new WaitForSeconds (Random.Range(0f,.2f));
